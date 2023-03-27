99fa6b3fa3b644ee116fd5d4b4dbce51a2eb15a0
Luis Rodrigues <luis.rodrigues@wunderkind.co>
2023-03-27 12:18:38 -0300

    solving 'num in list' and 'sum list of numbers'

--- module01/num_in_list.go
+++ module01/num_in_list.go
@@ -3,5 +3,16 @@ package module01
 // NumInList will return true if num is in the
 // list slice, and false otherwise.
 func NumInList(list []int, num int) bool {
+
+	if len(list) == 0 {
+		return false
+	}
+
+	for _, x := range list {
+		if x == num {
+			return true
+		}
+	}
+
 	return false
 }
