538ba5c08d19ab3d3292310c5515504c015cd90d
Luis Rodrigues <luis.rodrigues@wunderkind.co>
2023-03-27 15:05:34 -0300

    solving reverse a string (supporting runes)

--- module01/reverse.go
+++ module01/reverse.go
@@ -1,11 +1,22 @@
 package module01
 
+import (
+	"strings"
+)
+
 // Reverse will return the provided word in reverse
 // order. Eg:
 //
-//   Reverse("cat") => "tac"
-//   Reverse("alphabet") => "tebahpla"
-//
+//	Reverse("cat") => "tac"
+//	Reverse("alphabet") => "tebahpla"
 func Reverse(word string) string {
-	return ""
+
+	var rWord = []rune(word)
+	var sbReverse = strings.Builder{}
+
+	for i := len(rWord) - 1; i >= 0; i-- {
+		sbReverse.WriteRune(rWord[i])
+	}
+
+	return sbReverse.String()
 }
