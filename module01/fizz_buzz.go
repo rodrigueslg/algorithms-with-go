629b178d871ec8ca4ecff61ebac541a729d3b03c
Luis Rodrigues <luis.rodrigues@wunderkind.co>
2023-03-27 16:15:20 -0300

    solving fizz buzz

--- module01/fizz_buzz.go
+++ module01/fizz_buzz.go
@@ -1,5 +1,7 @@
 package module01
 
+import "fmt"
+
 // FizzBuzz will print out all of the numbers
 // from 1 to N replacing any divisible by 3
 // with "Fizz", and divisible by 5 with "Buzz",
@@ -11,4 +13,27 @@ package module01
 // I wouldn't normally recommend this, but did
 // it here to make life easier for beginners.
 func FizzBuzz(n int) {
+
+	for i := 1; i <= n; i++ {
+		fizz := i%3 == 0
+		buzz := i%5 == 0
+
+		if fizz && buzz {
+			fmt.Print("Fizz Buzz")
+		} else if fizz {
+			fmt.Print("Fizz")
+		} else if buzz {
+			fmt.Print("Buzz")
+		} else {
+			fmt.Print(i)
+		}
+
+		if i < n {
+			fmt.Print(", ")
+		}
+
+		if i == n {
+			fmt.Print("\n")
+		}
+	}
 }
