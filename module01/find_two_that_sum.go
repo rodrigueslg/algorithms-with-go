e9c309ac4c51b7971a29d61abea1198c5777a277
Luis Rodrigues <luis.rodrigues@wunderkind.co>
2023-03-28 17:12:47 -0300

    solving Find two that sum

--- module01/find_two_that_sum.go
+++ module01/find_two_that_sum.go
@@ -9,16 +9,16 @@ package module01
 //
 // Eg:
 //
-//   FindTwoThatSum([1,2,3,4], 7) => (2, 3)
-//   FindTwoThatSum([0, -1, 1], 0) => (1, 2)
-//   FindTwoThatSum([0, 1, 1], 0) => (-1, -1)
+//	FindTwoThatSum([1,2,3,4], 7) => (2, 3)
+//	FindTwoThatSum([0, -1, 1], 0) => (1, 2)
+//	FindTwoThatSum([0, 1, 1], 0) => (-1, -1)
 //
 // Or if we have duplicate answers any of them are okay. All
 // of the following are correct.
 //
-//   FindTwoThatSum([10, 1, 12, 3, 7, 2, 2, 1], 4) => (5, 6)
-//   FindTwoThatSum([10, 1, 12, 3, 7, 2, 2, 1], 4) => (1, 3)
-//   FindTwoThatSum([10, 1, 12, 3, 7, 2, 2, 1], 4) => (1, 7)
+//	FindTwoThatSum([10, 1, 12, 3, 7, 2, 2, 1], 4) => (5, 6)
+//	FindTwoThatSum([10, 1, 12, 3, 7, 2, 2, 1], 4) => (1, 3)
+//	FindTwoThatSum([10, 1, 12, 3, 7, 2, 2, 1], 4) => (1, 7)
 //
 // Each number will only be used once, and the original numbers
 // list should not be rearranged or altered in any way.
@@ -27,13 +27,26 @@ package module01
 // yourself further, try adding different criteria for which
 // numbers will be used when there are multiple correct answers.
 // Eg:
-// 1. Always return the lowest index possible for the first value.
-// 2. Always return the index of lowest value possible for the
-//    first return value.
-// 3. Always return the index of the two values who have a minimal
-//    difference. Eg prefer the values 2, 2 over 1, 3 over 0, 4
-//    for the sum of 4.
-//
+//  1. Always return the lowest index possible for the first value.
+//  2. Always return the index of lowest value possible for the
+//     first return value.
+//  3. Always return the index of the two values who have a minimal
+//     difference. Eg prefer the values 2, 2 over 1, 3 over 0, 4
+//     for the sum of 4.
 func FindTwoThatSum(numbers []int, sum int) (int, int) {
-	return 0, 0
+	var first, second = -1, -1
+
+	for i := 0; i < len(numbers); i++ {
+		first := numbers[i]
+
+		for j := i + 1; j < len(numbers); j++ {
+			second := numbers[j]
+
+			if first+second == sum {
+				return i, j
+			}
+		}
+	}
+
+	return first, second
 }
