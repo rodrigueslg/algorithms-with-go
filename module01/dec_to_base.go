7f6a60b3866663baa4ae5fdcb1e2e582dbc0444c
Luis Rodrigues <luis.rodrigues@wunderkind.co>
2023-03-28 09:43:08 -0300

    solving DecToBase

--- module01/dec_to_base.go
+++ module01/dec_to_base.go
@@ -1,14 +1,68 @@
 package module01
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 // DecToBase will return a string representing
 // the provided decimal number in the provided base.
 // This is limited to bases 2-16 for simplicity.
 //
 // Eg:
 //
-//   DecToBase(14, 16) => "E"
-//   DecToBase(14, 2) => "1110"
-//
+//	DecToBase(14, 16) => "E"
+//	DecToBase(14, 2) => "1110"
 func DecToBase(dec, base int) string {
-	return ""
+
+	if base > 16 {
+		println("max base is 16")
+		return ""
+	}
+
+	var result strings.Builder
+	calc(dec, base, &result)
+
+	//return Reverse(result.String())
+	return result.String()
+}
+
+func calc(dec, base int, sbResult *strings.Builder) int {
+	if dec == 0 {
+		return 0
+	}
+
+	reminder := dec % base
+	dec = dec / base
+
+	// save the current new base number string
+	current := sbResult.String()
+
+	// erase the builder
+	sbResult.Reset()
+
+	// append the new digit to the left
+	sbResult.WriteString(fmt.Sprintf("%s%s", numberToLetter(reminder), current))
+
+	return calc(dec, base, sbResult)
+}
+
+func numberToLetter(n int) string {
+	switch n {
+	case 10:
+		return "A"
+	case 11:
+		return "B"
+	case 12:
+		return "C"
+	case 13:
+		return "D"
+	case 14:
+		return "E"
+	case 15:
+		return "F"
+	default:
+		return strconv.Itoa(n)
+	}
 }
