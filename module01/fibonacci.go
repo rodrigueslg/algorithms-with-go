300cc09770e7537d46d3bda5aa4c6e67309b1550
Luis Rodrigues <luis.rodrigues@wunderkind.co>
2023-03-29 17:42:39 -0300

    solving fibonacci with memoization

--- module01/fibonacci.go
+++ module01/fibonacci.go
@@ -4,26 +4,38 @@ package module01
 //
 // A Fibonacci number N is defined as:
 //
-//   Fibonacci(N) = Fibonacci(N-1) + Fibonacci(N-2)
+//	Fibonacci(N) = Fibonacci(N-1) + Fibonacci(N-2)
 //
 // Where the following base cases are used:
 //
-//   Fibonacci(0) => 0
-//   Fibonacci(1) => 1
-//
+//	Fibonacci(0) => 0
+//	Fibonacci(1) => 1
 //
 // Examples:
 //
-//   Fibonacci(0) => 0
-//   Fibonacci(1) => 1
-//   Fibonacci(2) => 1
-//   Fibonacci(3) => 2
-//   Fibonacci(4) => 3
-//   Fibonacci(5) => 5
-//   Fibonacci(6) => 8
-//   Fibonacci(7) => 13
-//   Fibonacci(14) => 377
-//
+//	Fibonacci(0) => 0
+//	Fibonacci(1) => 1
+//	Fibonacci(2) => 1
+//	Fibonacci(3) => 2
+//	Fibonacci(4) => 3
+//	Fibonacci(5) => 5
+//	Fibonacci(6) => 8
+//	Fibonacci(7) => 13
+//	Fibonacci(14) => 377
 func Fibonacci(n int) int {
-	return 0
+	if n == 0 || n == 1 {
+		return n
+	}
+
+	var before = 0  // 1
+	var current = 1 // 2
+	var fib int
+
+	for i := 2; i <= n; i++ {
+		fib = before + current
+		before = current
+		current = fib
+	}
+
+	return fib
 }
