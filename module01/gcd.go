ab71c360bfb96c62fd87c66bf1cf6f7d6e4b5775
Luis Rodrigues <luis.rodrigues@wunderkind.co>
2023-03-30 12:00:52 -0300

    solving gcd

--- module01/gcd.go
+++ module01/gcd.go
@@ -1,5 +1,29 @@
 package module01
 
 func GCD(a, b int) int {
-	return 0
+	// var minor = a
+	// if b < a {
+	// 	minor = b
+	// }
+
+	// for i := minor; i > 1; i-- {
+	// 	modA := a % i
+	// 	modB := b % i
+	// 	if modA == 0 && modB == 0 {
+	// 		return i
+	// 	}
+	// }
+
+	// return 1
+
+	return GCDEuclideanAlgorithm(a, b)
 }
+
+func GCDEuclideanAlgorithm(a, b int) int {
+	for b != 0 {
+		a, b = b, a%b
+	}
+	return a
+}
+
+// GCD Euclidean Algorithm
