aa19dd13b078b0b701d59e2e73ff2dbe46cb5486
Luis Rodrigues <luis.rodrigues@wunderkind.co>
2023-03-30 09:56:32 -0300

    solving prime factoring

--- module01/factor.go
+++ module01/factor.go
@@ -11,16 +11,31 @@ package module01
 //
 // Examples:
 //
-//   Factor([]int{2,3,5}, 30) // []int{2,3,5}
-//   Factor([]int{2,3,5}, 28) // []int{2,2,7}
-//   Factor([]int{2,3,5}, 720) // []int{2,2,2,2,3,3,5}
+//	Factor([]int{2,3,5}, 30) // []int{2,3,5}
+//	Factor([]int{2,3,5}, 28) // []int{2,2,7}
+//	Factor([]int{2,3,5}, 720) // []int{2,2,2,2,3,3,5}
 //
 // Examples with remainders:
 //
-//   Factor([2,5], 30) // []int{2,5,3}
-//   Factor([3,5], 720) // []int{3,3,5,16}
-//   Factor([], 4) // []int{4}
-//
+//	Factor([2,5], 30) // []int{2,5,3}
+//	Factor([3,5], 720) // []int{3,3,5,16}
+//	Factor([], 4) // []int{4}
 func Factor(primes []int, number int) []int {
-	return nil
+	var factors []int
+
+	for i := 0; i < len(primes); {
+		mod := number % primes[i]
+		if mod == 0 {
+			factors = append(factors, primes[i])
+			number = number / primes[i]
+			continue
+		}
+		i++
+	}
+
+	if number > 1 {
+		factors = append(factors, number)
+	}
+
+	return factors
 }
